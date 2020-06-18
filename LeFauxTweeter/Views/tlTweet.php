61a5de2d6dd049e48ddcb4cd6d90a010b6d85fcf
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 19:03:28 +0200

    rajout de commetaires

--- LeFauxTweeter/Views/tlTweet.php
+++ LeFauxTweeter/Views/tlTweet.php
@@ -1,7 +1,6 @@
-<!-- Tweet apparaissant dans notre tl-->
-
+<!-- Tweets apparaissants dans notre timeline -->
 <article>
-    <h8>Utilisateur qui a tweeté</h8><h8>-</h8><h8>date</h8>
+    <h8>Utilisateur qui a tweetÃ©</h8><h8>-</h8><h8>date</h8>
     <h9>Contenu</h9>
     <h10>Retweeter </h10><h11>nombre </h11><h12>J'aime </h12><h13>nombre</h13>
 </article>
