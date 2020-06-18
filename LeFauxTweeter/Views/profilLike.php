2f78f8f46c30190a6f758e2c9b47592a9f4b5ae6
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 18:48:48 +0200

    rajout commentaires

--- LeFauxTweeter/Views/profilLike.php
+++ LeFauxTweeter/Views/profilLike.php
@@ -3,39 +3,40 @@
 <head>
     <meta charset="utf-8" />
     <title>Twitter</title>
+    <!-- insertion de la partie css -->
     <link rel="stylesheet"  href="css/styleProfil.css">
 </head>
 
 <body>
 
-<!--menu-->
-
+<!-- inclusion du menu -->
 <?php include("menu.php"); ?>
-
-<!-- Petit menu: tweets, abonnements, abonnés, j'aime-->
-
+    
+<!-- Bandeau de tweets, abonnements, abonnés, j'aime et edit -->
 <section>
-    <a href="profilTweet.php"><h4>Tweets</h4></a><a href="profilAbonnement.php"><h4>Abonnements</h4></a><a href="profilAbonnes.php"><h4>Abonnés</h4></a><a href="profilLike.php"><h4>J'aime</h4></a><a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
+    <a href="profilTweet.php"><h4>Tweets</h4></a>
+    <a href="profilAbonnement.php"><h4>Abonnements</h4></a>
+    <a href="profilAbonnes.php"><h4>Abonnés</h4></a>
+    <a href="profilLike.php"><h4>J'aime</h4></a>
+    <a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
 </section>
 
-<!-- courte description du profil-->
-
+<!-- inclusion de description profil-->
 <?php include("descriptionProfil.php"); ?>
 
-<!-- nos tweets et retweet-->
-
+<!--Section de tweets et retweet-->
 <?php foreach ($params['tweets'] as $tweets) : ?>
 
     <li>
         <?php if ($tweets["tweet_id"] == $_SESSION['id']) {?> <a href="Le-Faux-Tweeter/traitementSuppTweet/<?php echo $tweets["tweet_id"] ?>">  <h15>Supprimer</h15></a> <?php }?>
         <h8><?php echo $tweets["user_name"]; ?></h8><h10><?php echo $tweets["tweet_date"];?></h10>
         <h9><?php echo $tweets["tweet_content"]; ?></h9>
-        <a href="/Le-Faux-Tweeter/traitementRetweet/<?php echo $tweets["tweet_id"] ?>">  <h10>Retweeter</h10> </a><a href="traitementLike.php/<?php echo $tweets["tweet_id"] ?>"><h12>J'aime </h12> </a>
+        <a href="/Le-Faux-Tweeter/traitementRetweet/<?php echo $tweets["tweet_id"] ?>">  <h10>Retweeter</h10> </a>
+        <a href="traitementLike.php/<?php echo $tweets["tweet_id"] ?>"><h12>J'aime </h12> </a>
     </li>
 <?php endforeach; ?>
 
-<!-- Le pied de page -->
-
+<!-- inclusion du pied de page -->
 <?php include("pied_de_page.php"); ?>
 
 </body>
