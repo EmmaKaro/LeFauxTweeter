e3ccda5c37835ef4e9eff3c3b81889e2418bcbab
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 18:27:40 +0200

    rajout de commentaires

--- LeFauxTweeter/Views/descriptionProfil.php
+++ LeFauxTweeter/Views/descriptionProfil.php
@@ -1,10 +1,8 @@
-<!--description du profil-->
-
+<!-- Description d'un profil user -->
 
 <aside>
     <h6><?php echo $params['user']['pseudo'] ?></h6>
     <h7>@<?php echo $params['user']['user_name'] ?></h7>
     <h7><?php echo $params['user']['info_perso'] ?></h7>
-    <h7>Né le <?php echo $params['user']['birth'] ?></h7>
-
+    <h7>NÃ© le <?php echo $params['user']['birth'] ?></h7>
 </aside>
