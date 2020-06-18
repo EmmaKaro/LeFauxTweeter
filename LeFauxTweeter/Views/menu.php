f82217247a168b6ce52d42af0d9cf7de3cdbe50e
KarolaneP <61752489+KarolaneP@users.noreply.github.com>
2020-06-18 21:24:17 +0200

    cfgyuj

--- LeFauxTweeter/Views/menu.php
+++ LeFauxTweeter/Views/menu.php
@@ -2,7 +2,7 @@
 <nav id="menu">
     <ul class="form-style-1">
         <a href="tl.php"><h1>Accueil</h1></a>
-        <form action="/traitementRecherche.php" method="post">
+        <form action="indefiniMalheureusement" method="post">
             <p>
                 <input type="text" name="recherche" id="recherche" class="fiel-divided" placeholder="Recherchez sur Twitter"  />
                 <input type="submit" value="Rechercher" />
