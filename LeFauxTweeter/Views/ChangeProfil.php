767dfc5c47335de21646a3182126b89f39fa7302
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 18:22:48 +0200

    rajout de commentaires

--- LeFauxTweeter/Views/ChangeProfil.php
+++ LeFauxTweeter/Views/ChangeProfil.php
@@ -36,8 +36,11 @@
     </form>
 </article>
 
+    <!-- inclusion des images -->
+    <img class="phoenix1" src="img/Phoenix_right.png" HEIGHT="350" alt="un phoenix a droite de l'ecran">
+    <img class="phoenix2" src="img/Phoenix_left.png" HEIGHT="350" alt="un phoenix a gauche de l'ecran">
+    
 <!-- inclusion du pied de page -->
-
 <?php include("pied_de_page.php"); ?>
 
 </body>
