3aa16210745f72384dd9eea0ac877244ab182587
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 18:25:31 +0200

    rajout de commentaires

--- LeFauxTweeter/Views/Twitter.php
+++ LeFauxTweeter/Views/Twitter.php
@@ -3,20 +3,22 @@
 <head>
     <meta charset="utf-8" />
     <title>Twitter</title>
+    <!-- insertion de la partie css -->
     <link rel="stylesheet"  href="css/styleTwitter.css">
 </head>
 
 <body>
 
-<!--menu-->
-
+<!-- inclusion du menu -->
 <?php include("menu.php"); ?>
 
+<!-- Message 1 -->
 <h1>
   Vous avez quelque chose à partager ? </br>
   Dites-nous tout !
 </h1>
     
+<!-- Message 2 -->
 <article>
     <h4>Ecrire un nouveau Tweet</h4>
     <ul class="form-style-2">
@@ -25,13 +27,13 @@
         <input type="submit" value="Tweeter" />
     </ul>
     </form>
-
 </article>
 
+<!-- inclusion des images -->
 <img class="phoenix1" src="img/Phoenix_right.png" HEIGHT="350" alt="un phoenix a droite de l'ecran">
 <img class="phoenix2" src="img/Phoenix_left.png" HEIGHT="350" alt="un phoenix a gauche de l'ecran">
-<!-- Le pied de page -->
 
+<!-- inclusion du pied de page -->
 <?php include("pied_de_page.php"); ?>
 
 </body>
