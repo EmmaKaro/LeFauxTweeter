d2d1aaa2136f210798d63b80881aa9ecb7a2d97f
KarolaneP <61752489+KarolaneP@users.noreply.github.com>
2020-06-18 15:49:14 +0200

    n

--- LeFauxTweeter/Views/profilAbonnes.php
+++ LeFauxTweeter/Views/profilAbonnes.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Twitter</title>
-    <link rel="stylesheet"  href="css/styleProfilAbonnements.css">
+    <link rel="stylesheet"  href="css/styleProfilAbonnes.css">
 </head>
 
 <body>
