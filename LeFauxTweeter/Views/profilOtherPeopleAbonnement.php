f3bd793904a1d1a08dd0116ff1e5434475c606dc
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 18:50:31 +0200

    rajout commentaires

--- LeFauxTweeter/Views/profilOtherPeopleAbonnement.php
+++ LeFauxTweeter/Views/profilOtherPeopleAbonnement.php
@@ -3,38 +3,39 @@
 <head>
     <meta charset="utf-8" />
     <title>Twitter</title>
+    <!-- insertion de la partie css -->
     <link rel="stylesheet"  href="css/styleProfil.css">
 </head>
 
 <body>
 
-<!--menu-->
-
+<!-- inclusion du menu -->
 <?php include("menu.php"); ?>
 
-<!-- Petit menu: tweets, abonnements, abonnés, j'aime-->
-
+<!-- Bandeau de tweets, abonnements, abonnés, j'aime et edit -->
 <section>
-    <a href="profilTweet.php"><h4>Tweets</h4></a><a href="profilAbonnement.php"><h4>Abonnements</h4></a><a href="profilAbonnes.php"><h4>Abonnés</h4></a><a href="profilLike.php"><h4>J'aime</h4></a><a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
+    <a href="profilTweet.php"><h4>Tweets</h4></a>
+    <a href="profilAbonnement.php"><h4>Abonnements</h4></a>
+    <a href="profilAbonnes.php"><h4>Abonnés</h4></a>
+    <a href="profilLike.php"><h4>J'aime</h4></a>
+    <a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
 </section>
 
-<!-- courte description du profil-->
-
+<!-- inclusion de description profil-->
 <?php include("descriptionProfil.php"); ?>
 
-<!-- nos tweets et retweet-->
-
+<!-- Section de tweets et retweet-->
 <?php foreach ($params['tweets'] as $tweet) : ?>
 
     <li>
         <h8><?php echo $tweet["user_name"]; ?></h8><h10><?php echo $tweet["tweet_date"];?></h10>
         <h9><?php echo $tweet["tweet_content"]; ?></h9>
-        <a href="/Le-Faux-Tweeter/traitementRetweet/<?php echo $tweet["tweet_id"] ?>">  <h10>Retweeter</h10> </a><a href="/Le-Faux-Tweeter/traitementLike/<?php echo $tweet["tweet_id"] ?>">  <h12>J'aime </h12> </a>
+        <a href="/Le-Faux-Tweeter/traitementRetweet/<?php echo $tweet["tweet_id"] ?>">  <h10>Retweeter</h10> </a>
+        <a href="/Le-Faux-Tweeter/traitementLike/<?php echo $tweet["tweet_id"] ?>">  <h12>J'aime </h12> </a>
     </li>
 <?php endforeach; ?>
 
-<!-- Le pied de page -->
-
+<!-- inclusion du pied de page -->
 <?php include("pied_de_page.php"); ?>
 
 </body>
