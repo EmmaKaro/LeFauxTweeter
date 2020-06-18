e704202be6cc83255719ffec94b718ca8824a3a7
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-18 18:54:55 +0200

    rajout de commentaires

--- LeFauxTweeter/Views/profilOtherPeopleLike.php
+++ LeFauxTweeter/Views/profilOtherPeopleLike.php
@@ -3,31 +3,31 @@
 <head>
     <meta charset="utf-8" />
     <title>Twitter</title>
+    <!-- insertion de la partie css -->
     <link rel="stylesheet"  href="css/styleProfil.css">
 </head>
 
 <body>
 
-<!--menu-->
-
+<!-- inclusion du menu -->
 <?php include("menu.php"); ?>
 
-<!-- Petit menu: tweets, abonnements, abonnés, j'aime-->
-
+<!-- Bandeau de tweets, abonnements, abonnés, j'aime et edit -->
 <section>
-    <a href="profilTweet.php"><h4>Tweets</h4></a><a href="profilAbonnement.php"><h4>Abonnements</h4></a><a href="profilAbonnés.php"><h4>Abonnés</h4></a><a href="profilLike.php"><h4>J'aime</h4></a>
+    <a href="profilTweet.php"><h4>Tweets</h4></a>
+    <a href="profilAbonnement.php"><h4>Abonnements</h4></a>
+    <a href="profilAbonnés.php"><h4>Abonnés</h4></a>
+    <a href="profilLike.php"><h4>J'aime</h4></a>
+    <a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
 </section>
-
-<!-- courte description du profil-->
-
+    
+<!-- inclusion de description profil -->
 <?php include("descriptionProfil.php"); ?>
 
-<!-- nos tweets et retweet-->
-
+<!-- inclusion de la timeline -->
 <?php include ("tlTweet.php"); ?>
 
-<!-- Le pied de page -->
-
+<!-- inclusion du pied de page -->
 <?php include("pied_de_page.php"); ?>
 
 </body>
