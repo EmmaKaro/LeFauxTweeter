3e61a43058d93a8fba3076029c6296fdce945fc0
KarolaneP <61752489+KarolaneP@users.noreply.github.com>
2020-06-18 21:23:27 +0200

    hgd

--- LeFauxTweeter/Views/indexe.php
+++ LeFauxTweeter/Views/indexe.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="utf-8" />
     <title>Twitter</title>
-    <!-- insertion de la partie css -->
     <link rel="stylesheet"  href="css/style.css">
 </head>
 
 <body>
-    <!-- Connection au site -->
+
     <form method="post" action="/Le-Faux-Tweeter/traitementConnection">
         <ul class="form-style-1">
             <p>
@@ -19,7 +18,6 @@
         </ul>
     </form>
     <div class="flash">
-    <!-- sécurité session -->
     <?php if(isset($_SESSION['flash'])) {
         echo "
            <p style='color: red'>
@@ -30,20 +28,16 @@
      ?>
     </div>
 
-    <!-- inclusion des images -->
     <img class="phoenix1" src="img/Phoenix_right.png" HEIGHT="350" alt="un phoenix a droite de l'ecran">
     <img class="phoenix2" src="img/Phoenix_left.png" HEIGHT="350" alt="un phoenix a gauche de l'ecran">
 
-    <!-- iMessage 1 -->
     <h1>Le Twitter version Leader Price.</h1>
-    
-    <!-- Message 2 -->
+
     <h2>
       Vous n'avez pas encore de compte ? </br>
       Rejoignez notre faux Twitter dès aujourd'hui !
     </h2>
 
-    <!-- inscription au site -->
     <a href="inscription.php"><h3>S'inscrire</h3></a>
 
 
